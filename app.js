166f8aa06d14a8eeba8f2cfb7c3616c988e39788
Alessandro <33810424+ACINIELLO@users.noreply.github.com>
2021-05-09 17:53:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 
+class App(){
+   constructor() {
+        // Creating a container for the application
+
+        const container = document.createElement('div2');
+        document.body.appendChild(container);
+   }
+  function echoAR(var API_key)
+{
+ 
+var echoDB;
+
+// Query echoAR
+fetch('https://console.echoar.xyz/query?key=' + API_KEY)
+.then((response) => response.json())
+.then((json) => {
+  // Store database
+  echoDB = json; // The JSON response
+})
+.catch((error) => {
+  console.error(error);
+});
+  
+  
+}
+}
+
+
+export { App };
